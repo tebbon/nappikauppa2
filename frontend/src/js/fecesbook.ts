69ac306227659493febfd1dca7fdb4371a0d44d2
Teppo Nurminen <teppo@rakettitiede.com>
2016-08-22 23:38:13 +0300

    Add google events also

--- frontend/src/js/fecesbook.ts
+++ frontend/src/js/fecesbook.ts
@@ -1,7 +1,9 @@
 'use strict';
 
 declare function fbq(action: string, event: string, details: any): void;
+declare function ga(action: string, event: string, eventCategory: string, eventAction: string, eventLabel?: string, eventValue?: number): void;
 
 export function track(event: string, details?: any) {
     fbq('track', event, details);
+    ga('send', 'event', 'purchase', event);
 };
